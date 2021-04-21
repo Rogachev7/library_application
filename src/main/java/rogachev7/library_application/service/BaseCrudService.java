a7eedb8d823bd87145a5dbe5f766c349620ee41c
Rogachev7 <Rogachev7@users.noreply.github.com>
2021-04-21 17:54:55 +0300

    v.0.8 + add AbstractEntity + add CommonRepository + add BaseCrudService

---
+++ src/main/java/rogachev7/library_application/service/BaseCrudService.java
@@ -0,0 +1,43 @@
+package rogachev7.library_application.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import rogachev7.library_application.exception.EntityNotFoundException;
+import rogachev7.library_application.model.AbstractEntity;
+import rogachev7.library_application.repository.CommonRepository;
+
+import java.util.List;
+
+public abstract class BaseCrudService <E extends AbstractEntity, R extends CommonRepository<E>> {
+
+    protected final R repository;
+
+    @Autowired
+    public BaseCrudService(R repository) {
+        this.repository = repository;
+    }
+
+    public List<E> getAll() {
+        return repository.findAll();
+    }
+
+    public E getById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+    }
+
+    public E create(E entity) {
+        return repository.save(entity);
+    }
+
+    public E edit(E entity) {
+        return repository.save(entity);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public boolean existsById(Long id) {
+        return repository.existsById(id);
+    }
+
+}

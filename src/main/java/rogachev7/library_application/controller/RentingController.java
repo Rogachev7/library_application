1f5c879e127385f6cb186397f8f79cab77d5a68f
Rogachev7 <Rogachev7@users.noreply.github.com>
2021-06-29 20:44:21 +0300

    v.0.9 + add RentingDTO + add MapperDTO * edit controller layer * edit entity layer * edit service layer

---
+++ src/main/java/rogachev7/library_application/controller/RentingController.java
@@ -0,0 +1,41 @@
+package rogachev7.library_application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import rogachev7.library_application.model.DTO.RentingDTO;
+import rogachev7.library_application.model.DTO.mapper.RentingMapperDTO;
+import rogachev7.library_application.model.entity.Renting;
+import rogachev7.library_application.service.RentingService;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/renting")
+public class RentingController {
+
+    @Autowired
+    private RentingService rentingService;
+    @Autowired
+    private RentingMapperDTO rentingMapperDTO;
+
+    @GetMapping
+    public List<Renting> getAll() {
+        return rentingService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Renting get (@PathVariable Long id) {
+        return rentingService.getById(id);
+    }
+
+    @PostMapping
+    public Renting create (@RequestBody RentingDTO rentingDTO) {
+        return rentingService.create(rentingMapperDTO.getRentingEntity(rentingDTO));
+    }
+
+    @DeleteMapping("{id}")
+    public void delete (@PathVariable Long id) {
+        rentingService.deleteById(id);
+    }
+}
---
+++ src/main/java/rogachev7/library_application/model/DTO/mapper/RentingMapperDTO.java
@@ -0,0 +1,48 @@
+package rogachev7.library_application.model.DTO.mapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import rogachev7.library_application.exception.EntityNotFoundException;
+import rogachev7.library_application.model.DTO.RentingDTO;
+import rogachev7.library_application.model.entity.AbstractEntity;
+import rogachev7.library_application.model.entity.Book;
+import rogachev7.library_application.model.entity.Client;
+import rogachev7.library_application.model.entity.Renting;
+import rogachev7.library_application.repository.BookRepository;
+import rogachev7.library_application.repository.ClientRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RentingMapperDTO {
+
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public RentingDTO getRentingDTO(Renting entity) {
+        RentingDTO dto = new RentingDTO(entity.getDate());
+
+        Long clientId = entity.getClient().getId();
+        List<Long> booksId = entity.getBooks().stream().map(AbstractEntity::getId).collect(Collectors.toList());
+
+        dto.setClientId(clientId);
+        dto.setBooksId(booksId);
+
+        return dto;
+    }
+
+    public Renting getRentingEntity(RentingDTO dto) {
+        Renting entity = new Renting(dto.getDate());
+
+        Client client = clientRepository.getOne(dto.getClientId());
+        List<Book> books = dto.getBooksId().stream().map(id -> bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Book not found"))).collect(Collectors.toList());
+
+        entity.setClient(client);
+        entity.setBooks(books);
+
+        return entity;
+    }
+}
